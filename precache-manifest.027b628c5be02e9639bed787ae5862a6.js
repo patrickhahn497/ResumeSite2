7e501fb797a63a521c1f0ea5d53f0a8f53ce6ced
patrickhahn497 <patrickhahn497@gmail.com>
2020-04-29 18:27:48 -0700

    Updates

---
+++ precache-manifest.027b628c5be02e9639bed787ae5862a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf65a8f3360349e1b0b8b787db13bf1",
+    "url": "/ResumeSite2/index.html"
+  },
+  {
+    "revision": "352b7670c597878178e2",
+    "url": "/ResumeSite2/static/css/main.ee86ca27.chunk.css"
+  },
+  {
+    "revision": "931cbb98810cebf5dcb7",
+    "url": "/ResumeSite2/static/js/2.1f71585c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ResumeSite2/static/js/2.1f71585c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "352b7670c597878178e2",
+    "url": "/ResumeSite2/static/js/main.3c650bde.chunk.js"
+  },
+  {
+    "revision": "4a14e3177a60383e3c3f",
+    "url": "/ResumeSite2/static/js/runtime-main.7f9f1380.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ResumeSite2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6c04210406c18d02a80b3836c5b61261",
+    "url": "/ResumeSite2/static/media/starrysky2.6c042104.jpg"
+  }
+]);
